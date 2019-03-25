ed9d5f9b6e1edf545d6f6982272d4453cbea2b8e
nikkizip <45891798+nikkizip@users.noreply.github.com>
2019-03-25 18:17:37 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express')
 var app = express()
 
 app.get('/', function (req, res) {
-  res.send('Hello zippyops')
+  res.send('Hello zip')
 })
 
 app.listen(3000, function () {
